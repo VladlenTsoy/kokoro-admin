5cdaa5551bb1a5f439cb5a218bb8450cfd31c873
vladlentsoy <vladlengg@gmail.com>
2025-09-24 03:26:10 +0500

    update

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import {StrictMode} from "react"
+import {createRoot} from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import "antd/dist/reset.css"
+import "@ant-design/v5-patch-for-react-19"
+import {ConfigProvider, theme} from "antd"
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ConfigProvider
+            theme={{
+                algorithm: theme.defaultAlgorithm,
+                token: {
+                    fontFamily: "Averta CY, sans-serif",
+                    colorPrimary: "#B1E81C",
+                    borderRadius: 12
+                },
+                components: {
+                    Layout: {
+                        headerBg: "#fff"
+                    },
+                    Segmented: {
+                        itemSelectedBg: "#B1E81C",
+                        // itemSelectedColor: "#fff",
+                        // itemColor: "#fff",
+                        trackBg: "#FAFAFB"
+                    }
+                }
+            }}
+        >
+            <App />
+        </ConfigProvider>
+    </StrictMode>
+)
